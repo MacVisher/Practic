b64ab4072ad7c0f37ddbc6363a6e10f2e951b2d4
MacVisher <161427915+MacVisher@users.noreply.github.com>
2024-03-06 17:48:49 +0600

    Add files via upload

--- P_D/2/Java/java.js
+++ P_D/2/Java/java.js
@@ -1,11 +1,9 @@
-ymaps.ready(init);
-function init() {
+ymaps.ready(function() {
     var myMap = new ymaps.Map("map", {
         center: [49.813056, 73.105861],
         zoom: 19
     });
 
-    
     var customLayout = ymaps.templateLayoutFactory.createClass(
         '<div class="myPlacemarkLayout">' +
             '<img src="$[properties.iconContent]" alt="Изображение">' +
@@ -14,12 +12,10 @@ function init() {
     );
 
     var placemark1 = new ymaps.Placemark([49.813092, 73.105773], {
-        
         hintContent: 'Unit 40 - YAKIMA',
         balloonContent: '',
         iconContent: '../img/pizza.svg' 
     }, {
-        
         iconLayout: customLayout,
         iconShape: {
             type: 'Rectangle',
@@ -29,6 +25,40 @@ function init() {
 
     // Добавляем метку на карту
     myMap.geoObjects.add(placemark1);
-}
+    
+    // Добавляем обработчик события для кнопки
+    var asideSlideButton = document.getElementById("aside_slide_button");
+    var aside = document.getElementById("aside");
 
+    asideSlideButton.addEventListener("click", function() {
+        aside.classList.toggle("collapsed");
+    });
+});
+ymaps.ready(function() {
+    var myMap = new ymaps.Map("map", {
+        center: [49.813056, 73.105861],
+        zoom: 19
+    });
 
+    var customLayout = ymaps.templateLayoutFactory.createClass(
+        '<div class="myPlacemarkLayout">' +
+            '<img src="$[properties.iconContent]" alt="Изображение">' +
+            '<div>$[properties.balloonContent]</div>' +
+        '</div>'
+    );
+
+    var placemark1 = new ymaps.Placemark([49.813092, 73.105773], {
+        hintContent: 'Unit 40 - YAKIMA',
+        balloonContent: '',
+        iconContent: '../img/pizza.svg' 
+    }, {
+        iconLayout: customLayout,
+        iconShape: {
+            type: 'Rectangle',
+            coordinates: [[-25, -25], [25, 25]]
+        }
+    });
+
+    // Добавляем метку на карту
+    myMap.geoObjects.add(placemark1);
+    
