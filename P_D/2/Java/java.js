29967d97259500ca068058d83e50fe723c557d8c
MacVisher <161427915+MacVisher@users.noreply.github.com>
2024-03-14 17:07:45 +0500

    Add files via upload

--- P_D/2/Java/java.js
+++ P_D/2/Java/java.js
@@ -33,32 +33,12 @@ ymaps.ready(function() {
     asideSlideButton.addEventListener("click", function() {
         aside.classList.toggle("collapsed");
     });
+// Получаем кнопку и текст
+var button = document.querySelector(".menu_butt");
+var outputText = document.querySelector("#outputText");
+
+// Добавляем обработчик события на нажатие кнопки
+button.addEventListener("click", function() {
+    // Тогглим класс "active", который управляет видимостью текста
+    outputText.classList.toggle("active");
 });
-ymaps.ready(function() {
-    var myMap = new ymaps.Map("map", {
-        center: [49.813056, 73.105861],
-        zoom: 19
-    });
-
-    var customLayout = ymaps.templateLayoutFactory.createClass(
-        '<div class="myPlacemarkLayout">' +
-            '<img src="$[properties.iconContent]" alt="Изображение">' +
-            '<div>$[properties.balloonContent]</div>' +
-        '</div>'
-    );
-
-    var placemark1 = new ymaps.Placemark([49.813092, 73.105773], {
-        hintContent: 'Unit 40 - YAKIMA',
-        balloonContent: '',
-        iconContent: '../img/pizza.svg' 
-    }, {
-        iconLayout: customLayout,
-        iconShape: {
-            type: 'Rectangle',
-            coordinates: [[-25, -25], [25, 25]]
-        }
-    });
-
-    // Добавляем метку на карту
-    myMap.geoObjects.add(placemark1);
-    
